3a4bef12c85f6fd392b4916bd04658e48225e771
justicegarner24 <gar17042@byui.edu>
2024-05-08 09:50:40 -0600

    adding file journal entry

---
+++ prove/Develop02/JournalEntry.cs
@@ -0,0 +1,10 @@
+using System;
+
+public class JournalEntry
+{
+    public string _entryNumber = "";
+    public string _dateTime = "";
+    public string _journalPrompt = "";
+    public string _journalEntry = "";
+    public string _journalFile = "";
+}
