319ddee279370a8997124a7c6db0458771ad32f7
justicegarner24 <gar17042@byui.edu>
2024-05-22 11:04:34 -0600

    rftghjnbv

--- prove/Develop03/Program.cs
+++ prove/Develop03/Program.cs
@@ -5,8 +5,8 @@ class Program
 {
     static void Main(string[] args)
     {
-        Reference scriptureReference = new Reference("Proverbs", "3", "5-6");
-        Scripture scripture = new Scripture(scriptureReference, "Trust in the lord with all thine heart and understanding; in all thy ways acknowledge him, and he shall direct thy paths.");
+        Reference scriptureReference = new Reference("James", "1", "5");
+        Scripture scripture = new Scripture(scriptureReference, "5 If any of you lack wisdom, let him ask of God, that giveth to all men liberally, and upbraideth not; and it shall be given him.");
         ScriptureMemorizer scriptureMemorizer = new ScriptureMemorizer(scripture);
 
         string userInput = "";
